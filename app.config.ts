f93c26c782ccad7dea3eaea3282b64983df0575e
Ana Robles <ana.robles@hyd.com.es>
2024-09-28 11:58:29 +0200

    Add new component for testing purposes

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,27 @@
+import { field, group } from "@nuxthq/studio/theme";
 export default defineAppConfig({
-  // Configure Content Wind here
-  cover: '/cover.jpg',
-  socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind',
-    mastodon: '@atinux@webtoo.ls'
-  }
-})
+	// Configure Content Wind here
+	cover: "/cover.jpg",
+	socials: {
+		twitter: "Atinux",
+		github: "Atinux/content-wind",
+		mastodon: "@atinux@webtoo.ls",
+	},
+	appConfig: {
+		parent: group({
+			title: "UI",
+			description: "UI configuration",
+			icon: "i-ph-palette-fill",
+			fields: {
+				primary: field({
+					type: "string",
+					title: "Primary",
+					description: "Primary color of your UI.",
+					icon: "i-ph-palette",
+					default: "sky",
+					required: ["sky", "mint", "rose", "amber"],
+				}),
+			},
+		}),
+	},
+});
---
+++ components/content/TitleSubtitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>
+        <ContentSlot name="title" />
+    </h1>
+    <h2>
+        <ContentSlot name="subtitle" />
+    </h2>
+</template>
+
+<script lang="ts" setup>
+    defineProps({
+        title: String,
+        subtitle: String
+    })
+</script>
+
+<style lang="css" scoped>
+    h1 {
+        color: var(--color-primary, #C70039);
+    }
+
+    h2 {
+        color: var(--color-secondary, #581845);
+    }
+</style>
